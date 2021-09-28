7383743747ce8ffada4f389727d199e0c97dce6a
Jessica Hicks <jhicks1@ufl.edu>
2021-09-28 15:23:43 -0600

    September updates

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('index.html')
+# @app.route('/about')
+# def about():
+#     return render_template('index.html')
 
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
 
-@app.route('/cv')
-def cv():
-    return render_template('cv.html')
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
 
 @app.route('/contact')
 def contact():
