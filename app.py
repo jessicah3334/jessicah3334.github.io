de6c045a7dc44098ec84d0aff17b67f1e31c94a8
Jessica Hicks <jhicks1@ufl.edu>
2021-09-28 20:28:40 -0600

    style update and new portfolio layout

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+# App Routes for main pages
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# @app.route('/about')
-# def about():
-#     return render_template('index.html')
 
 @app.route('/portfolio')
 def portfolio():
@@ -22,3 +19,20 @@ def resume():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+# App Routes for portfolio pages
+@app.route('/engineering')
+def engineering():
+    return render_template('portfolio-items/eng.html')
+
+@app.route('/data')
+def data():
+    return render_template('portfolio-items/data.html')
+
+@app.route('/software')
+def software():
+    return render_template('portfolio-items/software.html')
+
+@app.route('/volunteering')
+def volunteering():
+    return render_template('portfolio-items/volunteer.html')
